861ef59b76824e07b600526d9e5e9fe142ae62e5
anii76 <a.bourouina@esi-sba.dz>
2021-03-24 14:46:28 +0100

    Launcher icon & splash screen

--- app/src/main/java/com/createch/meublessalsabil/MainActivity.java
+++ app/src/main/java/com/createch/meublessalsabil/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_MeublesSalsabil);
         setContentView(R.layout.login_layout);
     }
 }
